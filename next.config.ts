952a34d1266969449b2c13ea0d63fe517dd731ff
BLCompany1 <conteudobl@gmail.com>
2026-02-09 15:33:57 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
+module.exports = nextConfig
